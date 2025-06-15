31619462b9701aaae79d0c48c68495ad57dc0062
duimond <Duimond@yandex.ru>
2025-06-15 23:23:34 +0300

    font

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import Header from "./components/Header"
 import Users from "./components/Users"
 import Autorization from "./components/Autorization"
 class App extends React.Component {
+    constructor (props){
+        super(props)
+        this.state = {
+            id: 0,
+            nick: "",
+            rank: "",
+            squad: 0,
+            squadRank: "",
+            data: new Date(),
+            role: "",
+            roleInSite: ""
+        }
+    }
     render () {
         return(
             <div>
--- src/components/Autorization.js
+++ src/components/Autorization.js
@@ -1,14 +1,21 @@
 import React from "react";
 import Logo from '../icons/logo.png'
 class Autorization extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+        nick: "",
+        password: ""
+    }
+  }
   render(){
     return(
       <div className="autorization"> 
       <img src={Logo} className="logo"/>
       <aside>
       <form>
-        <input placeholder="ник сюды" />
-        <input placeholder="пароль вот сюды" />
+        <input placeholder="ник сюды"  onChange={(e) => this.setState({nick: e.target.nick})}/>
+        <input placeholder="пароль вот сюды" onChange={(e) => this.setState({password: e.target.password})}/>
         <button>Войти</button>
       </form>
       </aside>
