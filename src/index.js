ef535ab0390ffad9888b11cb6c6469395e7b630b
duimond <Duimond@yandex.ru>
2025-06-15 13:47:29 +0300

    welcome

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
+import App from "./components/App"
+import './css/main.css'
 
-const inputClick = () => console.log("Нажатие")
-const mouseOver = () => console.log("мышка")
-const helpful = "Привет расплатовец"
-const elements = (<div className = "name">
-  <h1>{helpful}</h1>
-  <input placeholder={helpful} onClick={inputClick} onMouseOver={mouseOver}/>
-</div>)
+
+
+
+
+
 const licat = ReactDOMClient.createRoot(document.getElementById("app"))
-licat.render(elements)
+licat.render(<App/>)
